3cb8109f9f24e8919ee98f55edc92c7b463c213d
Abdulaziz <Abdulaziz>
2023-02-07 16:17:24 +0600

    зделал budget

---
+++ src/js/main.js
@@ -0,0 +1,31 @@
+//const money = prompt("Ваш бюджет на месяц?");
+
+//onst time = prompt("Введите дату в формате YYYY-MM-DD");
+//
+//onst appData = {
+// budget: money,
+// timeData: time,
+// expenses: {},
+// optionalExpenses: null,
+// income: null,
+// savings: false,
+//;
+//
+//or (let i = 0; i < 2; i++) {
+// let q1 = prompt("Введите обязательную статью расходnв в этом месяце", ""),
+//   q2 = prompt("Во сколько обойдется", "");
+//
+// if (
+//   typeof q1 === "string" &&
+//   q1 != "" &&
+//   typeof q1 != null &&
+//   typeof q2 === "number" &&
+//  q2 != ""
+//) {
+//  appData.expenses[q1] = q2;
+//}
+//console.log(appData);
+
+
+
+
